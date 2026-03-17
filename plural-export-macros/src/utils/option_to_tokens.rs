3934a1a7198ff5f3fb5462d2f67b7db1fd1ef2a6
tyrantlink <38902185+tyrantlink@users.noreply.github.com>
2026-03-17 01:59:03 -0700

    remove antcore macro utils

--- plural-export-macros/src/utils/option_to_tokens.rs
+++
@@ -1,7 +0,0 @@
-use proc_macro2::TokenStream;
-use quote::{ToTokens, quote};
-
-
-pub fn option_to_tokens<T: ToTokens>(option: Option<&T>) -> TokenStream {
-    option.map_or_else(|| quote! { None }, |option| quote! { Some(#option) })
-}
